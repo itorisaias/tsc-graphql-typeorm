dcf82f6f02b957ef2126f70005140d3388b34a08
itor isaias da silva <itor.isaias@gmail.com>
2020-08-12 00:44:13 -0300

    remove log and add callback success server

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,9 @@ import { connect } from '@src/config/typeorm';
 async function main() {
   connect();
   const server = await startServer();
-  server.listen(3000);
-  console.log('> [server] listen on port', 3000);
+  server.listen(3000, () => {
+    console.log('> [server] listen on port', 3000);
+  });
 }
 
 main();
--- src/utils/module-alias.ts
+++ src/utils/module-alias.ts
@@ -2,7 +2,7 @@ import * as paths from 'path';
 import moduleAlias from 'module-alias';
 
 const files = paths.resolve(__dirname, '../..');
-console.log({ files });
+
 moduleAlias.addAliases({
   '@src': paths.join(files, 'src'),
 });
